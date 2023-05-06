42bb77531c29e9a5679799f52b3251dec6968012
Micdu70 <Micdu70@gmx.fr>
2023-05-06 09:12:22 +0200

    Update to v1.167 (hotfix)

--- Views/LevelDetails.cs
+++ Views/LevelDetails.cs
@@ -283,7 +283,6 @@ namespace FallGuysStats {
             this.gridDetails.Columns["InParty"].Visible = false;
             this.gridDetails.Columns["PrivateLobby"].Visible = false;
             this.gridDetails.Columns["Qualified"].Visible = false;
-            this.gridDetails.Columns["DidFinish"].Visible = false;
             this.gridDetails.Columns["IsFinal"].Visible = false;
             this.gridDetails.Columns["IsTeam"].Visible = false;
             if (this._showStats == 0) {
