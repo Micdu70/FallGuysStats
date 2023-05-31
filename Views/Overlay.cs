04148b674b4a4c3ce693d245bd495888b12eb852
eunma <terro21c@gmail.com>
2023-06-01 00:10:01 +0900

    Bug fix

--- Views/Overlay.cs
+++ Views/Overlay.cs
@@ -130,25 +130,14 @@ namespace FallGuysStats {
 
         public Overlay() {
             this.InitializeComponent();
+        }
+
+        private void Overlay_Load(object sender, EventArgs e) {
             this.ChangeLanguage();
-
-            //this.picPositionNE.Image = this.positionNeOffBlur;
-            //this.picPositionNW.Image = this.positionNwOffBlur;
-            //this.picPositionSE.Image = this.positionSeOffBlur;
-            //this.picPositionSW.Image = this.positionSwOffBlur;
-            //this.picPositionLock.Image = this.positionUnlockBlur;
-
-            //this.picPositionNE.Location = new Point((this.Width / 2) - (this.picPositionNE.Size.Width + 2), (this.Height / 2) - (this.picPositionNE.Size.Height + 2));
-            //this.picPositionNW.Location = new Point((this.Width / 2) + 2, (this.Height / 2) - (this.picPositionNW.Size.Height + 2));
-            //this.picPositionSE.Location = new Point((this.Width / 2) - (this.picPositionSE.Size.Width + 2), (this.Height / 2) + 2);
-            //this.picPositionSW.Location = new Point((this.Width / 2) + 2, (this.Height / 2) + 2);
-            //this.picPositionLock.Location = new Point(this.Location.X - 2, this.Location.Y - 8);
-
-            //SetFonts(this);
-
             this.SetBackground();
             this.SetStyle(ControlStyles.ResizeRedraw, true);
         }
+
         public void SetFixedPosition(bool positionNe, bool positionNw, bool positionSe, bool positionSw, bool positionFree) {
             this.isFixedPositionNe = positionNe;
             this.isFixedPositionNw = positionNw;
